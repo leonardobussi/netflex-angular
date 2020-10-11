585fb89347f5a5d166e73c2df88596b8b5ed4cc8
leonardobussi <leonardobezerrabussy@gmail.com>
2020-10-11 10:38:40 -0300

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { FullBannerComponent } from './shared/full-banner/full-banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FullBannerComponent
   ],
   imports: [
     BrowserModule,
