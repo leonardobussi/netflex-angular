ac1afa6f4e251995edc8f6bb477ff1d56f82ee18
leonardobussi <leonardobezerrabussy@gmail.com>
2020-10-11 17:41:52 -0300

    heh

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -16,10 +16,17 @@ export class TitleComponent implements OnInit {
 
 
   playVideo(){
-    this.trailer.nativeElement.currentTime = 0;
+   
     this.trailer.nativeElement.play();
   }
   stopVideo(){
+    this.trailer.nativeElement.currentTime = 0;
     this.trailer.nativeElement.pause();
+  }
+  getMuted(): boolean {
+    return this.trailer.nativeElement.muted;
+  }
+  toggleSound(){
+    this.trailer.nativeElement.muted = !this.trailer.nativeElement.muted;
   }
 }
