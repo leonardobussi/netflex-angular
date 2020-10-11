856fda6e797416f3a18c5365490c25f450bc683d
leonardobussi <leonardobezerrabussy@gmail.com>
2020-10-11 13:33:22 -0300

    concertado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  listTitles = [{
+      trailer: 'assets/videos/cap.mp4',
+      cover: 'assets/images/guardios-da-galaxia.jpg',
+      name: 'Guardiões da galaxia',
+      relevance: 98,
+      age: 12,
+      parts: 2,
+      categories: ['Épico', 'Filme fantasia', 'viagem no espaço']
+    },{
+      trailer: 'assets/videos/cap.mp4',
+      cover: 'assets/images/guardios-da-galaxia.jpg',
+      name: 'Hora de cagar',
+      relevance: 0,
+      age: 18,
+      parts: 1,
+      categories: ['cagada', 'Filme', 'caganeira']
+    },{
+      trailer: 'assets/videos/cap.mp4',
+      cover: 'assets/images/guardios-da-galaxia.jpg',
+      name: 'sua maẽ é uma trapper',
+      relevance: 100,
+      age: 0,
+      parts: 16,
+      categories: ['nada sei', 'vai saber', 'top']
+    },{
+      trailer: 'assets/videos/cap.mp4',
+      cover: 'assets/images/guardios-da-galaxia.jpg',
+      name: 'sua maẽ é uma trapper 2',
+      relevance: 90,
+      age: 0,
+      parts: 10,
+      categories: ['nada sei', 'vai saber', 'top']
+    }
+  ]
+
 }
