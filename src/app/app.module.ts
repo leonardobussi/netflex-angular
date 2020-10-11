3e190a26860844a5f9456376d2dbedbd106a8067
leonardobussi <leonardobezerrabussy@gmail.com>
2020-10-11 11:03:29 -0300

    add component footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 import { FullBannerComponent } from './shared/full-banner/full-banner.component';
+import { ListTitlesComponent } from './shared/list-titles/list-titles.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    FullBannerComponent
+    FullBannerComponent,
+    ListTitlesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
